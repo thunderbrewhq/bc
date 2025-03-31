61faa87c316bea320b2a94a4efbf82bff725a000
superp00t <superp00t@tutanota.com>
2025-03-30 23:36:05 -0400

    feat(test): add tests for new allocation helpers

--- test/Memory.cpp
+++ test/Memory.cpp
@@ -1,6 +1,19 @@
 #include "bc/Memory.hpp"
 #include "test/Test.hpp"
 
+#include <cstdio>
+
+struct TestObject {
+    int m = 1;
+    TestObject() {
+        // printf("TestObject created\n");
+    }
+
+    ~TestObject() {
+        // printf("TestObject destroyed\n");
+    }
+};
+
 TEST_CASE("Blizzard::Memory::Allocate", "[memory]") {
     SECTION("allocates memory and returns pointer") {
         auto ptr = Blizzard::Memory::Allocate(100);
@@ -16,5 +29,25 @@ TEST_CASE("Blizzard::Memory::Allocate", "[memory]") {
         REQUIRE(ptr);
 
         Blizzard::Memory::Free(ptr);
+    }
+}
+
+TEST_CASE("helper macros", "[memory]") {
+    SECTION("ALLOCATE memory and return pointer") {
+        auto m = ALLOC(128);
+        REQUIRE(m);
+        FREE(m);
+    }
+
+    SECTION("NEW object and DELETE it") {
+        auto m = NEW(TestObject);
+        DEL(m);
+    }
+}
+
+TEST_CASE("operator new", "[memory]") {
+    SECTION("allocate memory through the new operator") {
+        auto m = new uint8_t[0xCAFE];
+        delete m;
     }
 }
